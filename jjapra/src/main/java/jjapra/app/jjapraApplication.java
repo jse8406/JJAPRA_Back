97c6dff07296209055455e4cdf2e81aca4c334af
jse <73538772+jse8406@users.noreply.github.com>
2024-05-14 16:26:56 +0900

    init

---
+++ jjapra/src/main/java/jjapra/app/jjapraApplication.java
@@ -0,0 +1,13 @@
+package jjapra.app;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class jjapraApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(jjapraApplication.class, args);
+    }
+}
