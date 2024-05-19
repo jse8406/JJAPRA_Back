5e714b165ce8ee54ed5a3eb4de87578834f95afe
Haemulramen <apple11078@gmail.com>
2024-05-19 21:34:58 +0900

    REFACTOR: add Join controller into Member controller

--- jjapra/src/main/java/jjapra/app/controller/MemberController.java
+++ jjapra/src/main/java/jjapra/app/controller/MemberController.java
@@ -1,18 +1,35 @@
 package jjapra.app.controller;
 
+import jjapra.app.dto.AddMemberRequest;
 import jjapra.app.model.Member;
 import jjapra.app.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 public class MemberController {
     private final MemberService memberService;
+
+    @RequestMapping(value = "/join")
+    public String displayJoinPage() {
+        return "join.html";
+    }
+
+    @PostMapping(value = "/join")
+    public ResponseEntity<Object> displayJoinSuccessPage(@RequestBody AddMemberRequest request) {
+        if (request.getId().isEmpty()){
+            return ResponseEntity.badRequest().body(null);
+        }
+        if (memberService.findById(request.getId()) != null) {
+            return ResponseEntity.badRequest().body("이미 존재하는 아이디입니다.");
+        }
+
+        memberService.save(request);
+        return ResponseEntity.ok().body("회원가입이 완료되었습니다.");
+    }
 
     @RequestMapping(value = "/member/{id}", method = RequestMethod.GET)
     public Member getMember(@PathVariable("id") String id) {
