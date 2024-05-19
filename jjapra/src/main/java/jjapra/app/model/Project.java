15128490891c56bf414ecddd763fc7b9292cb78f
Haemulramen <apple11078@gmail.com>
2024-05-19 16:20:10 +0900

    CHORE: create Project

---
+++ jjapra/src/main/java/jjapra/app/model/Project.java
@@ -0,0 +1,24 @@
+package jjapra.app.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Embeddable
+public class Project {
+    @Id
+    @Column(nullable = false, length = 30, unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false, length = 30, unique = true)
+    private String title;
+    @Column
+    private String description;
+}
