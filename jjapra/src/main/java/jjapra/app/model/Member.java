acc9d1b4349093084c2e4765891000da98eab6e0
Haemulramen <apple11078@gmail.com>
2024-05-21 21:44:41 +0900

    FEAT: member class is_admin column

--- jjapra/src/main/java/jjapra/app/model/Member.java
+++ jjapra/src/main/java/jjapra/app/model/Member.java
@@ -30,34 +30,6 @@ public class Member {
     private String email;
     @Column(nullable = false, length = 30)
     private String phone_num;
-
-//    @Override
-//    public Collection<? extends GrantedAuthority> getAuthorities() {
-//        return List.of(new SimpleGrantedAuthority("user"));
-//    }
-//
-//    @Override
-//    public String getUsername() {
-//        return id;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonExpired() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonLocked() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isCredentialsNonExpired() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isEnabled() {
-//        return true;
-//    }
+    @Column(nullable = false, columnDefinition = "boolean default false")
+    private boolean is_admin;
 }
