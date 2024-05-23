656d2608a2443fcd3a42be8b88b333a6ec99c79f
Haemulramen <apple11078@gmail.com>
2024-05-23 20:42:23 +0900

    FEAT: cors

--- jjapra/src/main/java/jjapra/app/config/WebMvcConfig.java
+++ jjapra/src/main/java/jjapra/app/config/WebMvcConfig.java
@@ -28,7 +28,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
                         "https://localhost:3000", "https://localhost:8080", "https://localhost:5500",
                         "http://127.0.0.1:3000", "http://127.0.0.1:8080", "http://127.0.0.1:5500",
                         "https://127.0.0.1:3000", "https://127.0.0.1:8080", "https://127.0.0.1:5500",
-                        "https://testtest3458.netlify.app", "https://jjapra.netlify.app"
+                        "https://testtest3458.netlify.app/", "https://jjapra.netlify.app",
+                        "https://suzzang2.github.io.kro.kr"
                         )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
