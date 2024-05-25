2e3a6f7efc13d52f5681ec9cfa1d7354e2efb1c4
Haemulramen <apple11078@gmail.com>
2024-05-25 23:35:57 +0900

    FEAT: cors

--- jjapra/src/main/java/jjapra/app/config/WebMvcConfig.java
+++ jjapra/src/main/java/jjapra/app/config/WebMvcConfig.java
@@ -1,27 +1,25 @@
-package jjapra.app.config;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
-
-    private final long MAX_AGE_SECS = 3600;
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://localhost:5500",
-                        "https://localhost:3000", "https://localhost:8080", "https://localhost:5500",
-                        "http://127.0.0.1:3000", "http://127.0.0.1:8080", "http://127.0.0.1:5500",
-                        "https://127.0.0.1:3000", "https://127.0.0.1:8080", "https://127.0.0.1:5500"
-                )
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(MAX_AGE_SECS);
-    }
-}
+//package jjapra.app.config;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebMvcConfig implements WebMvcConfigurer {
+//
+//    private final long MAX_AGE_SECS = 3600;
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://localhost:5500",
+//                        "http://127.0.0.1:3000", "http://127.0.0.1:8080", "http://127.0.0.1:5500"
+//                )
+//                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(true)
+//                .maxAge(MAX_AGE_SECS);
+//    }
+//}
